9e9cf628fa3a48b374ff8c73ab26676bf55cb85a
Shadman Ahmed <shadman.ahmed001@gmail.com>
2017-01-13 23:17:47 -0800

    first commit

---
+++ users.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: 
+-- Date: 2017-01-13 16:19
+*/
+INSERT INTO `users` (`id`,`name`) VALUES (1,'user1');
+INSERT INTO `users` (`id`,`name`) VALUES (2,'user2');
+INSERT INTO `users` (`id`,`name`) VALUES (3,'user3');
